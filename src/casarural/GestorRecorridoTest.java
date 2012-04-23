c0f6e1e719fbf4a15b3ee2341515622560cb6c1c
Ander Arbelaiz <arbelaitz@gmail.com>
2012-04-23 23:16:49 +0200

    Finalizado test de GestorRecorrido

--- src/casarural/GestorRecorridoTest.java
+++ src/casarural/GestorRecorridoTest.java
@@ -13,30 +13,74 @@ public class GestorRecorridoTest extends TestCase {
 	protected void setUp() throws Exception {
 		this.GR = GestorRecorrido.getInstance();
 	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
+	
+	/**Prueba para verificar la instancia de GestorRecorrido
+	 * 
 	 */
-	protected void tearDown() throws Exception {
-		this.GR = null;
+	public void testGetInstance() {
+		assertSame(this.GR, GestorRecorrido.getInstance());
 	}
 	
-	public void testGetInstance() {
-		//fail("Not yet implemented");
-	}
-
+	/**Prueba del método GetCodigoCasas
+	 * Verifica que se recibe la primera casa rural de la BD inicial
+	 */
 	public void testGetCodigoCasas() {
+		//Creamos una Casa rural ficticia que esta en la BD inicial
+		Casa miCasa = new Casa();
+		miCasa.setNumCasa(1);
+		miCasa.setPropietario("JoxeMari");
+		miCasa.setNumDormitorios(3);
+		miCasa.setDescripcion("Casa en Gipuzkoa");
+		miCasa.setNumCocinas(2);
+		miCasa.setNumPlazasGaraje(0);
+		miCasa.setPoblacion("Donostia");
+		
 		List<Casa> listaDeCasas = GR.getCodigoCasas();
 		//Verificamos que efectivamente se ha devuelto una list no vacia
 		assertFalse(listaDeCasas.isEmpty());
+		//Verificamos que la primera casa esta en la lista recibida
+		boolean found = false;
+		for (Casa casa: listaDeCasas) {
+			if (miCasa.getNumCasa() == casa.getNumCasa())  found = true;
+		}
+		assertTrue(found);
 	}
 
+	/**
+	 * TestAsignarRecorrido
+	 * comprueba que se ha asignado correctamente un recorrido asignando uno
+	 * 
+	 */
 	public void testAsignarRecorrido() {
-		fail("Not yet implemented");
+		Casa miCasa = new Casa();
+		miCasa.setNumCasa(1);
+		miCasa.setPropietario("JoxeMari");
+		miCasa.setNumDormitorios(3);
+		miCasa.setDescripcion("Casa en Gipuzkoa");
+		miCasa.setNumCocinas(2);
+		miCasa.setNumPlazasGaraje(0);
+		miCasa.setPoblacion("Donostia");
+		
+		List<Casa> listaDeCasas = new ArrayList<Casa>();
+		listaDeCasas.add(miCasa);
+		
+		assertTrue(GR.asignarRecorrido(listaDeCasas));
 	}
 
+	/**
+	 * Test para probar GestoRecorridos 
+	 * solo funciona con los datos iniciales de la BD
+	 */
 	public void testGetRecorridos() {
-		fail("Not yet implemented");
+		List<Recorrido> listaDeRecorrido = GR.getRecorridos();
+		//Comprobar que la lista que se recive no es vacia
+		assertFalse(listaDeRecorrido.isEmpty());
+		//Comprobamos que existe el numRecorrido 1 en la lista de recorridos devuelta que está en la BD inicial 
+		boolean found = false;
+		for(Recorrido recorrido: listaDeRecorrido){
+			if(recorrido.getNumRecorrido() == 1) found = true;
+		}
+		assertTrue(found);
 	}
 
 }
