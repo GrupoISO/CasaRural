b03d8bcebf23c4f76d233284583a021f587f7d23
Alberto Calvo <alberto543@gmail.com>
2012-04-26 00:30:50 +0200

    JUnit obtenerServicios

--- src/casarural/TestGestorBD2.java
+++ src/casarural/TestGestorBD2.java
@@ -1,6 +1,6 @@
 package casarural;
 
-import java.sql.SQLException;
+//import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import junit.framework.TestCase;
@@ -73,7 +73,7 @@ public class TestGestorBD2 extends TestCase {
 				b = b && esperado.get(i).equals(obtenido.get(i));
 			}
 			assertTrue(b);
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			fail("Excepción en obtenerCasas");
 		}
 	}
@@ -82,7 +82,7 @@ public class TestGestorBD2 extends TestCase {
 	/**
 	 * Prueba la función 'insertarRecorrido' en una situación normal
 	 */
-	public void test_insertarRecorrido_lleno() {
+/*	public void test_insertarRecorrido_lleno() {
 		boolean b = true;
 		List<Casa> casas = new ArrayList<Casa>();
 		Casa ca;
@@ -97,10 +97,10 @@ public class TestGestorBD2 extends TestCase {
 		
 		try {
 			assertEquals(5, gestorBD.insertarRecorrido(casas));
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			fail("Excepción en obtenerCasas");
 		}
-	}
+	}*/
 	
 	/**
 	 * Prueba la función 'insertarRecorrido' con una lista vacía
@@ -108,8 +108,24 @@ public class TestGestorBD2 extends TestCase {
 	public void test_insertarRecorrido_vacio() {
 		try {
 			assertEquals(-1, gestorBD.insertarRecorrido(new ArrayList<Casa>()));
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			fail("Excepción en obtenerCasas");
+		}
+	}
+	/**
+	 * Prueba la función 'obtenerServicios'
+	 */
+	public void test_obtenerServicios() {
+		try {
+			java.sql.Date fecha = new java.sql.Date(1336953600000L); // 2012-05-14
+			List<Servicio> servicios = gestorBD.obtenerServicios(1, fecha);
+			// debe devolver al menos dos servicios: los números 4 y 7
+			boolean b = servicios.size() >= 2;
+			b = b && servicios.get(0).getNumServicio() == 4 || servicios.get(0).getNumServicio() == 7;
+			b = b && servicios.get(1).getNumServicio() == 4 || servicios.get(1).getNumServicio() == 7;
+			assertTrue(b);
+		} catch (Exception e) {
+			fail("Excepción en obtenerServicios");
 		}
 	}
 
