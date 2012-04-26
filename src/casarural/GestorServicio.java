863f9b5fb4eb53ba4a15580774e631fa9f672e91
Ander Arbelaiz <arbelaitz@gmail.com>
2012-04-27 00:45:51 +0200

    Corregido Transacción de Reserva
    Se ha utilizado la funcionalidad curte de utilizar el fichero que
    mantiene el numero de reserva actual, para mantener retrocompatibilidad
    con el programa

--- src/casarural/GestorServicio.java
+++ src/casarural/GestorServicio.java
@@ -1,5 +1,11 @@
 package casarural;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -8,6 +14,7 @@ public class GestorServicio {
 
 	private static GestorServicio gestorServicio;
 	private GestorBD gbd;
+	private static String ficheroNumReserva = "numeroReserva.txt";
 	
 	/**Devuelve una instancia de la clase GestorServicio
 	 * @param ninguno
@@ -24,6 +31,35 @@ public class GestorServicio {
 		gbd = GestorBD.getInstance();
 	}
 	
+	/**
+	 * Accede al fichero que contiene el numero de reserva
+	 * 
+	 * @param Ninguno
+	 * @return El numero de reserva
+	 */
+	protected static int cargarNumReserva() throws FileNotFoundException,
+	IOException {
+		BufferedReader entrada = new BufferedReader(new FileReader(
+				ficheroNumReserva));
+		int numero = Integer.parseInt(entrada.readLine());
+		entrada.close();
+		return numero;
+	}
+
+	/**
+	 * Guarda en el fichero el numero de reserva actual
+	 * 
+	 * @param Ninguno
+	 * @return Ninguno
+	 */
+	protected static void salvarNumReserva(int numactual) throws FileNotFoundException,
+	IOException {
+		PrintWriter numero = new PrintWriter(new FileWriter(ficheroNumReserva));
+		Integer num = new Integer(numactual);
+		numero.print(num.toString());
+		numero.close();
+	}
+
 	/**Obtiene una lista de las recogidas actuales
 	 * @param ninguno
 	 * @return Una lista de recogidas
@@ -67,15 +103,26 @@ public class GestorServicio {
 	 * @param numPlazas Numero de plazas que se quiere reservar
 	 * @return
 	 */
-	public boolean transaccionDeReserva(List<Oferta> reservasTotales, int idReserva, String numTfno, int idServicio, int numPlazas){
+	public boolean transaccionDeReserva(List<Oferta> reservasTotales, String numTfno, int idServicio, int numPlazas){
 		try {
+			int idReserva = cargarNumReserva() + 1;
 			float precioTotal = 0;
 			for(Oferta reservaOferta: reservasTotales){
 				precioTotal = precioTotal + reservaOferta.getPrecio();
 			}
 			gbd.transaccionDeReserva(reservasTotales, idReserva, numTfno, precioTotal, idServicio, numPlazas);
+			salvarNumReserva(idReserva);
 			return true;
-		}catch(SQLException ex){
+		}
+		catch(FileNotFoundException ex){
+			ex.printStackTrace();
+			return false;
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+			return false;
+		}
+		catch(SQLException ex){
 			ex.printStackTrace();
 			return false;
 		}
