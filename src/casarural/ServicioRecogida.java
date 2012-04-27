3199c1ac64eb90cacde56a122d5402230db0fb4e
Jon Aguirre <jonaguirre12@hotmail.com>
2012-04-27 11:30:32 +0200

    Anadido hora a ServicioRecogida.java (no funciona bien)

--- src/casarural/ServicioRecogida.java
+++ src/casarural/ServicioRecogida.java
@@ -20,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JComboBox;
 
 import com.toedter.calendar.JCalendar;
+import javax.swing.JTextField;
 
 
 public class ServicioRecogida extends JFrame {
@@ -32,6 +33,9 @@ public class ServicioRecogida extends JFrame {
 	private JFormattedTextField plazasTextField;
 	private JFormattedTextField precioTextField;
 	private JCalendar calendar;
+	private JFormattedTextField textHora;
+	private JFormattedTextField textMin;
+	private JLabel lblError;
 
 	/**
 	 * Launch the application.
@@ -51,7 +55,7 @@ public class ServicioRecogida extends JFrame {
 
 	public ServicioRecogida() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 510);
+		setBounds(100, 100, 451, 593);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -62,19 +66,19 @@ public class ServicioRecogida extends JFrame {
 		contentPane.add(calendar);
 		
 		JLabel lblRecorridos = new JLabel("Recorrido");
-		lblRecorridos.setBounds(152, 320, 84, 16);
+		lblRecorridos.setBounds(152, 307, 84, 16);
 		contentPane.add(lblRecorridos);
 		
 		recorridosComboBox = new JComboBox();
-		recorridosComboBox.setBounds(152, 348, 182, 27);
+		recorridosComboBox.setBounds(152, 325, 182, 27);
 		contentPane.add(recorridosComboBox);
 		
 		JLabel lblServ = new JLabel("Recogida");
-		lblServ.setBounds(152, 387, 84, 16);
+		lblServ.setBounds(152, 369, 84, 16);
 		contentPane.add(lblServ);
 		
 		recogidasComboBox = new JComboBox();
-		recogidasComboBox.setBounds(152, 415, 182, 27);
+		recogidasComboBox.setBounds(152, 396, 182, 27);
 		contentPane.add(recogidasComboBox);
 		
 		Button button = new Button("Confirmar");
@@ -83,11 +87,11 @@ public class ServicioRecogida extends JFrame {
 				finalCrearServicio();
 			}
 		});
-		button.setBounds(192, 448, 101, 28);
+		button.setBounds(192, 517, 101, 28);
 		contentPane.add(button);
 		
 		plazasTextField = new JFormattedTextField(NumberFormat.getIntegerInstance());
-		plazasTextField.setBounds(152, 280, 182, 28);
+		plazasTextField.setBounds(152, 268, 182, 28);
 		plazasTextField.setValue(new Integer(0));
 		contentPane.add(plazasTextField);
 		plazasTextField.setColumns(10);
@@ -105,10 +109,35 @@ public class ServicioRecogida extends JFrame {
 		contentPane.add(lblPrecio);
 		
 		precioTextField = new JFormattedTextField(NumberFormat.getInstance());
-		precioTextField.setBounds(152, 218, 182, 28);
+		precioTextField.setBounds(152, 213, 182, 28);
 		precioTextField.setValue(new Float(0.0f));
 		contentPane.add(precioTextField);
 		precioTextField.setColumns(10);
+		
+		textHora = new JFormattedTextField(NumberFormat.getInstance());
+		textHora.setText("23");
+		textHora.setBounds(186, 470, 50, 20);
+		textHora.setValue(new Integer(23));
+		contentPane.add(textHora);
+		textHora.setColumns(10);
+		
+		JLabel lblHora = new JLabel("Hora");
+		lblHora.setBounds(152, 445, 61, 14);
+		contentPane.add(lblHora);
+		
+		JLabel label = new JLabel(":");
+		label.setBounds(247, 473, 46, 14);
+		contentPane.add(label);
+		
+		textMin = new JFormattedTextField(NumberFormat.getInstance());
+		textMin.setValue(new Integer(59));
+		textMin.setBounds(257, 470, 50, 20);
+		contentPane.add(textMin);
+		textMin.setColumns(10);
+		
+		lblError = new JLabel("");
+		lblError.setBounds(10, 497, 415, 14);
+		contentPane.add(lblError);
 	}
 
 	public void crearServicio() {
@@ -141,18 +170,25 @@ public class ServicioRecogida extends JFrame {
 		servicio.setPrecio((Float)precioTextField.getValue());
 		servicio.setNumRecogida(((Recogida)recogidasComboBox.getSelectedItem()).getNumRecogida());
 		servicio.setNumRecorrido(((Recorrido)recorridosComboBox.getSelectedItem()).getNumRecorrido());
-		
-		GestorServicio gServicio = GestorServicio.getInstance();
-		
-		boolean result = gServicio.crearServicio(servicio);
-		
-		if (result) {
-			JOptionPane.showMessageDialog(this, "Se ha guardado con �xito",
-					"�xito al guardar", JOptionPane.INFORMATION_MESSAGE);
-			this.dispose();
-		} else {
-			JOptionPane.showMessageDialog(this, "Ha habido un error al guardar",
-					"Error al crear", JOptionPane.ERROR_MESSAGE);
+		if((Integer)textHora.getValue()<0 || (Integer)textHora.getValue()>23 || (Integer)textMin.getValue()<0 || (Integer)textMin.getValue()>59)
+		{
+			lblError.setText("Formato erroneo: Hora(0..23) Minutos(0..59");
+		}
+		else
+		{
+			servicio.setHora(java.sql.Time.valueOf((String)textHora.getValue() + ":" + (String)textMin.getValue()));
+			GestorServicio gServicio = GestorServicio.getInstance();
+
+			boolean result = gServicio.crearServicio(servicio);
+
+			if (result) {
+				JOptionPane.showMessageDialog(this, "Se ha guardado con �xito",
+						"�xito al guardar", JOptionPane.INFORMATION_MESSAGE);
+				this.dispose();
+			} else {
+				JOptionPane.showMessageDialog(this, "Ha habido un error al guardar",
+						"Error al crear", JOptionPane.ERROR_MESSAGE);
+			}
 		}
 	}
 }
