b1a9d09475bc2524c19f1fb63ed7fb63c58b531f
Ander Arbelaiz <arbelaitz@gmail.com>
2012-04-29 01:08:04 +0200

    Creado Bean para comprobar administrador

---
+++ src/casarural/ComprobarAdministradorBean.java
@@ -0,0 +1,36 @@
+package casarural;
+
+import java.rmi.*;
+
+public class ComprobarAdministradorBean {
+	
+	InterfazFachada logNeg;
+	
+	
+	/**Constructor de la clase CrearAdministradorBean
+	 * @param ninguno
+	 * @return constructor
+	 */
+	public ComprobarAdministradorBean(){
+		try
+	    {
+	      logNeg = ((InterfazFachada)Naming.lookup("rmi://localhost:1099/CasaRural"));
+	    }
+	    catch(Exception e) {System.out.println("Error al conseguir la logica del negocio: "+e.toString());}
+	}
+	
+	/**Método para comprobar la veracidad del administrador
+	 * 
+	 * @param usuario
+	 * @return
+	 */
+	public boolean esAdministrador(String usuario){
+		try{
+			return logNeg.esAdministrador(usuario);
+		}catch(Exception e){
+			e.getStackTrace();
+			return false;
+		}
+	}
+
+}
