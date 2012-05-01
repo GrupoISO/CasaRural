7e6d4527771f60f99a0cd50d70550e93c487d327
Gorka Maiztegi <gmaiztegi@gmail.com>
2012-05-01 16:27:42 +0200

    Javadoc de GestorAdministrador

--- src/casarural/GestorAdministrador.java
+++ src/casarural/GestorAdministrador.java
@@ -2,11 +2,18 @@ package casarural;
 
 import java.sql.SQLException;
 
+/**
+ * Gestor de administradores, para comprobar que existan en la base de datos.
+ */
 public class GestorAdministrador {
 	private static GestorAdministrador gestorAdministrador;
 	
 	private GestorBD gBd;
 	
+	/**
+	 * Obtiene la instancia del gestor de administradores.
+	 * @return La instancia del gestor
+	 */
 	public static GestorAdministrador getInstance() {
 		if (gestorAdministrador == null) {
 			gestorAdministrador = new GestorAdministrador();
@@ -19,6 +26,11 @@ public class GestorAdministrador {
 		gBd = GestorBD.getInstance();
 	}
 	
+	/**
+	 * Comprueba que el código sea de un administrador válido.
+	 * @param codigo El código de administrador
+	 * @return Verdadero si el administrador existe, falso en caso contrario.
+	 */
 	public boolean esAdministrador(String codigo) {
 		try {
 			return gBd.comprobarAdministrador(codigo);
